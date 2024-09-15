fff92c4e7f76c7b3174a5d5ad9f0f581545041c8
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-09-16 04:46:46 +0500

    cont..

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
-import { getAuth,
+import {
+  getAuth,
   onAuthStateChanged,
- } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-
-
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCfqNBhhWtn1wmzvPmGoXE5-hxviZtKlcA",
@@ -16,26 +14,52 @@ const firebaseConfig = {
   storageBucket: "sir-basit-firebase-pract-3b71c.appspot.com",
   messagingSenderId: "525801764690",
   appId: "1:525801764690:web:57fba62db30374af55ac8b",
-  measurementId: "G-YD85XC7BFZ"
+  measurementId: "G-YD85XC7BFZ",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// console.log(app);
-
 const auth = getAuth(app);
 
-// console.log(auth);
+const First_name = document.getElementById('First_name')
+const Last_name = document.getElementById('Last_name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const Signup_Btn = document.getElementById('Signup_Btn')
+
+Signup_Btn.addEventListener("click" , creatUserAccount);
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    ("User is logged in")
-   
+    console.log("User is logged in===>");
     const uid = user.uid;
-    
   } else {
-    ("User is not logged in")
-
+    console.log("User is not logged in===>");
   }
 });
+
+
+function creatUserAccount() {
+  // console.log("First_name", First_name.value);
+  // console.log("Last_name", Last_name.value);
+  // console.log("email", email.value);
+  // console.log("password", password.value);
+  createUserWithEmailAndPassword(
+    auth,
+     First_name.value, 
+      Last_name.value,
+        email.value,
+         password.value)
+
+  .then((userCredential) => {
+    
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert("errormessage");
+  });
+}
